7756a38806cb3495be3cb1c8c689e639d2a17609
DESKTOP-V78KGF4\LOVE <wkdskdud05@naver.com>
2021-07-26 09:59:10 +0900

    php오류 생성 시 처리부분 추가

--- h.php
+++ h.php
@@ -1,9 +1,25 @@
 <?php
-$m1 = file_get_contents('hh.json');
-$m2 = json_decode($m1);
 
-foreach($m2 as $value)
-{
-    echo $value.PHP_EOL;
-}
+    function isJson($string) {
+        json_decode($string);
+        return json_last_error() === JSON_ERROR_NONE;
+    }
+
+    
+
+    $m1 = file_get_contents('hh.djson');
+    if(isJson($m1)){
+        $m2 = json_decode($m1);
+        foreach($m2 as $value)
+        {
+            echo $value.PHP_EOL;
+        }
+    } else {
+            echo 'banana
+            apple';
+    }
+    
+    
+
+
 ?>
