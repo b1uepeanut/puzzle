ab72da1b5f907f59e0d5f415be6b80b4b6568e28
DESKTOP-V78KGF4\LOVE <wkdskdud05@naver.com>
2021-07-28 09:23:54 +0900

    inpput.php(입력값 전달)
    js input창에서 입력한 값을 php파일로 받아와서 그 값을 다시 data.json파일에 배열형태로 저장

--- input.php
+++ input.php
@@ -1,7 +1,46 @@
 <?php
-session_start();
-$a = $_POST[time];
-$b =  $_POST[name];
+$time = $_POST['time'];
+$name = $_POST['name'];
 
-echo $a, $b;
+var_dump($time);
+var_dump($name);
+
+if(file_exists('data.json')){
+    $json = file_get_contents('data.json');
+    $data = json_decode($json,true);
+    if(empty($data))
+    {
+        echo "안녕";
+        $data = array();
+        file_put_contents('data.json',json_encode($data,JSON_UNESCAPED_UNICODE));
+        $json = file_get_contents('data.json');
+        $data = json_decode($json,true);
+        $data1 = array($time => $name);
+        $data2 = array_merge($data,$data1);
+        echo "dks";
+        file_put_contents('data.json',json_encode($data2,JSON_UNESCAPED_UNICODE));
+    } else {
+        $data1 = array($time => $name);
+        $data2 = array_merge($data,$data1);
+        var_dump($data2);
+        file_put_contents('data.json',json_encode($data2,JSON_UNESCAPED_UNICODE));
+    }
+
+}
+
+// if(file_exists($json)){
+// $json = file_get_contents('data.json');
+//     $data = json_decode($json);
+//     $data = array($time => $name);
+//     array_push($json,$data);
+//    file_put_contents('data.json', json_encode($data));
+
+// } else {
+//     $json = file_get_contents('data.json');
+//     $data = json_decode($json);
+//     $data = array($time => $name);
+//    file_put_contents('data.json', json_encode($data,));
+
+// }
 ?>
+
