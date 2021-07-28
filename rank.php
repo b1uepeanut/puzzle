f5d6a3ae04d12209427985950c04a00172f913c7
DESKTOP-V78KGF4\LOVE <wkdskdud05@naver.com>
2021-07-28 14:41:35 +0900

    json파일에서 값 불러와서 순위 계산하는 php파일

--- rank.php
+++ rank.php
@@ -1,9 +1,25 @@
 <?php
-    $openFile = file_get_contents('data.json');
-    $getData = json_decode($openFile);
-rsort($getData);
 
-foreach($getData as $key=> $value)
-{
-    echo "시간=".$key." , 이름=".$value."<br>";
-}
+    
+    $openFile = file_get_contents('data.json');
+    $getData = json_decode($openFile,true);
+
+    ksort($getData);
+    $length =count($getData);
+
+    // var_dump($getData);
+    // krsort($getData);
+    // var_dump($getData);
+
+    $i = 0;
+
+    function plus()
+    {
+        $GLOBALS['i']  += 1;
+    }
+
+    foreach($getData as $key=> $value)
+    {
+        
+        echo plus().$i."위\t시간=".$key."  이름=".$value."<br>"; 
+    }
