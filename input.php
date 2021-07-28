eddb73255800f8c65bf5af4f3fa412a435de5693
DESKTOP-V78KGF4\LOVE <wkdskdud05@naver.com>
2021-07-28 14:40:32 +0900

    입력받은시간과 이름을 json형식으로 파일에 넣고, 그 후에 json파일을 가져와서 순위 계산 후 출력

--- input.php
+++ input.php
@@ -1,46 +1,30 @@
+
 <?php
 $time = $_POST['time'];
 $name = $_POST['name'];
-
-var_dump($time);
-var_dump($name);
+$arr = array($time => $name);
 
 if(file_exists('data.json')){
     $json = file_get_contents('data.json');
-    $data = json_decode($json,true);
-    if(empty($data))
+    $getData= json_decode($json,true);
+    if(empty($getData))
     {
         echo "안녕";
-        $data = array();
-        file_put_contents('data.json',json_encode($data,JSON_UNESCAPED_UNICODE));
-        $json = file_get_contents('data.json');
-        $data = json_decode($json,true);
-        $data1 = array($time => $name);
-        $data2 = array_merge($data,$data1);
-        echo "dks";
-        file_put_contents('data.json',json_encode($data2,JSON_UNESCAPED_UNICODE));
+        $getData= array();
+        $totalData = array_merge($getData,$arr);
+        file_put_contents('data.json',json_encode($totalData,JSON_UNESCAPED_UNICODE));
     } else {
-        $data1 = array($time => $name);
-        $data2 = array_merge($data,$data1);
-        var_dump($data2);
-        file_put_contents('data.json',json_encode($data2,JSON_UNESCAPED_UNICODE));
+        $totalData = array_merge($getData,$arr);
+        file_put_contents('data.json',json_encode($totalData,JSON_UNESCAPED_UNICODE));
     }
 
 }
 
-// if(file_exists($json)){
-// $json = file_get_contents('data.json');
-//     $data = json_decode($json);
-//     $data = array($time => $name);
-//     array_push($json,$data);
-//    file_put_contents('data.json', json_encode($data));
-
-// } else {
-//     $json = file_get_contents('data.json');
-//     $data = json_decode($json);
-//     $data = array($time => $name);
-//    file_put_contents('data.json', json_encode($data,));
-
-// }
 ?>
+
+<?php 
+        echo "<script type='text/javascript'> 
+        location.replace('rank.php') 
+            </script>"; 
+?> 
 
